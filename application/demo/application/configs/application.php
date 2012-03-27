4037a794c4a58f938579eb985226fc214294f70b
cadina <wudicadina@gmail.com>
2012-03-28 01:42:22 +0800

    remove all

--- application/demo/application/configs/application.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return array(
-	'siteUrl' => '',
-	'modules' => array(
-	),
-);
--- application/demo/application/configs/route.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return array(
-    'routes' => array(
-        'spec' => array('/post-<id:\d+>', 'home', 'post', array('<id>')),
-	    'default' => array('/<controller:\w+>/<action:\w+>', '<controller>', '<action>'),
-	),
-);
